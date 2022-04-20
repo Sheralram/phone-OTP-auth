44f59aa8701f70ff70f42419d0eedc3519ca7e1f
Sheralram <ramkrishna.sheral@gmail.com>
2022-04-20 12:39:40 +0530

    Added : Working properly

---
+++ phone-auth/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PhoneNumberComponent } from './phone-number/phone-number.component';
+import { CodeComponent } from './code/code.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FormsModule } from '@angular/forms';
+
+// Firebase services + enviorment module
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+
+import firebase from 'firebase';
+firebase.initializeApp(environment.firebase);
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PhoneNumberComponent,
+    CodeComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    FormsModule,   
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ phone-auth/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase:{
+  apiKey: "AIzaSyCasir0fbYnsg2dh6L1lVQVZPNtdakdYxk",
+  authDomain: "phone-auth-28953.firebaseapp.com",
+  projectId: "phone-auth-28953",
+  storageBucket: "phone-auth-28953.appspot.com",
+  messagingSenderId: "780238656715",
+  appId: "1:780238656715:web:b081542dfdeff1838cf74e"
+  }
+};
